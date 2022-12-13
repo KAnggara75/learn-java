63ed7ba96ff8a6e216e7c0115750a5280b20158c
Call Vin <kanggara75@gmail.com>
2022-12-13 15:29:54 +0700

    add: get post controller

---
+++ restful/src/main/java/me/kanggara/restful/controllers/TestController.java
@@ -0,0 +1,23 @@
+package me.kanggara.restful.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class TestController {
+    @GetMapping
+    public String welcome(){
+        return "Hello world!";
+    }
+
+    @PostMapping
+    public String other(){
+        return "This Is POST Method";
+    }
+
+
+}
